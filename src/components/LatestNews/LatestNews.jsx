7ee81170453cf456ad35bf659e7499595801764e
Ilya <emptydesert29@gmail.com>
2024-07-22 14:21:11 +0300

    add latest news

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,12 +1,14 @@
+import { getLatestNews } from "../../api/apiNews";
+import { useFetch } from "../../helpers/hooks/useFetch";
 import { BannersListWithSkeleton } from "../BannersList/BannersList";
 import styles from './styles.module.css'
 
-export const LatestNews = (props) => {
-  const { news, isLoading } = props;
+export const LatestNews = () => {
+  const { data: dataNews, isLoading } = useFetch(getLatestNews);
 
   return (
     <section className={styles.section}>
-        <BannersListWithSkeleton banners={news} isLoading={isLoading} />
+        <BannersListWithSkeleton banners={dataNews?.news} isLoading={isLoading} />
     </section>
   )
 }
