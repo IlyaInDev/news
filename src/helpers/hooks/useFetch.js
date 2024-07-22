775ff2c05c3d238b5c52acee72ebc81956fccf26
Ilya <emptydesert29@gmail.com>
2024-07-22 11:23:38 +0300

    add design for web

--- src/helpers/hooks/useFetch.js
+++ src/helpers/hooks/useFetch.js
@@ -3,25 +3,25 @@ import { useEffect, useState } from "react"
 export const useFetch = (fetchFunction, params) => {
     const [data, setData] = useState(null);
     const [error, setError] = useState(null);
-    const [loading, setLoading] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
     const stringParams = params ? new URLSearchParams(params).toString() : '';
 
     useEffect(() => {
       const fetchData = async () => {
-        setLoading(true);
+        setIsLoading(true);
         try {
           const result = await fetchFunction(params);
           setData(result);
         } catch (err) {
           setError(err);
         } finally {
-          setLoading(false);
+          setIsLoading(false);
         }
       };
   
       fetchData();
     }, [fetchFunction, stringParams]);
   
-    return { data, error, loading };
+    return { data, error, isLoading };
   };
