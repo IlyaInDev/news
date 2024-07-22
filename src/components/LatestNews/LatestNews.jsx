c173b1dfff86a2cd3ca9cdf219867d3b553dd272
IlyaInDev <156908348+IlyaInDev@users.noreply.github.com>
2024-07-22 14:05:51 +0300

    Revert "add latest news"

--- src/components/LatestNews/LatestNews.jsx
+++ src/components/LatestNews/LatestNews.jsx
@@ -1,14 +1,12 @@
-import { getLatestNews } from "../../api/apiNews";
-import { useFetch } from "../../helpers/hooks/useFetch";
 import { BannersListWithSkeleton } from "../BannersList/BannersList";
 import styles from './styles.module.css'
 
-export const LatestNews = () => {
-  const { data: dataNews, isLoading } = useFetch(getLatestNews);
+export const LatestNews = (props) => {
+  const { news, isLoading } = props;
 
   return (
     <section className={styles.section}>
-        <BannersListWithSkeleton banners={dataNews?.news} isLoading={isLoading} />
+        <BannersListWithSkeleton banners={news} isLoading={isLoading} />
     </section>
   )
 }
--- src/pages/Main/Main.jsx
+++ src/pages/Main/Main.jsx
@@ -1,13 +1,40 @@
+import { getNews } from '../../api/apiNews';
+import { useDebounce } from '../../helpers/hooks/useDebounce';
+import { PAGE_SIZE } from '../../constants/constants';
+import { useFetch } from '../../helpers/hooks/useFetch';
+import { useFilter } from '../../helpers/hooks/useFilter';
+import styles from './styles.module.css'
 import { LatestNews } from '../../components/LatestNews/LatestNews';
 import { NewsByFilters } from '../../components/NewsByFilters/NewsByFilters';
-import styles from './styles.module.css'
 
 export const Main = () => {
+  const { filters, changeFilter } = useFilter({
+    page_number: 1,
+    page_size: PAGE_SIZE,
+    category: null,
+    keywords: ''
+  })
+
+  const debouncedKeywords = useDebounce(filters.keywords, 1000);
+
+  const { data: dataNews, isLoading } = useFetch(getNews, {
+    ...filters,
+    keywords: debouncedKeywords
+  });
+
   return (
     <main className={styles.main}>
-        <LatestNews />
+        <LatestNews
+            news={dataNews?.news}
+            isLoading={isLoading}
+        />
 
-        <NewsByFilters />
+        <NewsByFilters
+            news={dataNews?.news}
+            filters={filters}
+            changeFilter={changeFilter}
+            isLoading={isLoading}
+        />
     </main>
   )
 }
